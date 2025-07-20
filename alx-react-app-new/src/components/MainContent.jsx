bfc72b5843df00faa151777705b5bb9a210cff3d
SAUMWANGEKA <146136230+SAUMWANGEKA@users.noreply.github.com>
2025-07-20 21:17:18 +0300

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa' }}>
+      <p style={{ fontSize: '1.2rem' }}>
+        Welcome to my list of favorite cities. Scroll down to meet our featured users!
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
