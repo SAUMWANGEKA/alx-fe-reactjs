4e5946dd543d68f155bc6e54b3d8ae0e92c68d3c
SAUMWANGEKA <146136230+SAUMWANGEKA@users.noreply.github.com>
2025-07-20 21:17:46 +0300

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', padding: '1rem', textAlign: 'center' }}>
+      <p>&copy; 2025 Roselyn's Travel Picks</p>
+    </footer>
+  );
+}
+
+export default Footer;
