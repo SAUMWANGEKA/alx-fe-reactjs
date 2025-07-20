d351de9e71ad5d07ca218e082a6c7eee1bed00bb
SAUMWANGEKA <146136230+SAUMWANGEKA@users.noreply.github.com>
2025-07-20 21:20:42 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,9 @@
 function MainContent() {
   return (
-    <main style={{ padding: '20px', backgroundColor: '#e0f7fa' }}>
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '150px', textAlign: 'center' }}>
+      <h2 style={{ color: 'darkslategray' }}>Explore My Favorite Cities</h2>
       <p style={{ fontSize: '1.2rem' }}>
-        Welcome to my list of favorite cities. Scroll down to meet our featured users!
+        Discover unique destinations, cultures, and people!
       </p>
     </main>
   );
