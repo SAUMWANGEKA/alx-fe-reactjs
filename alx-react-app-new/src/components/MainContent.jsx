0d81f3f9f42f316212e762069a14c5c82d7e6443
SAUMWANGEKA <146136230+SAUMWANGEKA@users.noreply.github.com>
2025-07-20 21:21:36 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,8 @@
 function MainContent() {
   return (
-    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '150px', textAlign: 'center' }}>
-      <h2 style={{ color: 'darkslategray' }}>Explore My Favorite Cities</h2>
-      <p style={{ fontSize: '1.2rem' }}>
-        Discover unique destinations, cultures, and people!
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', textAlign: 'center' }}>
+      <p style={{ fontSize: '1.2rem', color: 'darkslategray' }}>
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
